5e6998eb4754c8c2d2e804ac19e819b269f418c2
Bryan Ramos <bryan.ramos@hey.com>
2022-12-02 07:46:21 -0500

    Remove test vars and other junk

--- app.py
+++ app.py
@@ -1,60 +1,38 @@
 import re
 
-# Regex References
-###############################
-#
-
-heading = r"^=(.*)={1,}$"
-
-# heading 1, 2, 3...
-heading_2 = "^=(.*)={2}$"
-heading_3 = "^=(.*)={3}$"
-heading_4 = "^=(.*)={4}$"
-heading_5 = "^=(.*)={5}$"
-
-# Italic markup
-italic = "_(.*)_"
-
-# Inline code
-inline_code = "`(.*)`"
-
-# links
-basic_link = "\[\[(.*)\]\]"
-link_with_description = "\[\[((.*)\|{1}.*)\]\]"
-
-# file links
-file_link = "\{\{(file:.*)\}\}"
-
-# Code block
-code_block = "\{\{\{([^}]*)\}\}\}"
-
 REGEX = {
-    "heading"        : heading,
-    "italic"         : italic,
-    "inline_code"    : inline_code,
-    "basic_link"     : basic_link,
-    "file_link"      : file_link,
-    "code_block"     : code_block,
+    "heading"              : "^=(.*)={1,}$",
+    "italic"               : "_(.*)_",
+    "inline_code"          : "`(.*)`",
+    "basic_link"           : "\[\[(.*)\]\]",
+    "link_with_description": "\[\[((.*)\|{1}.*)\]\]",
+    "file_link"            : "\{\{(file:.*)\}\}",
+    "code_block"           : "\{\{\{([^}]*)\}\}\}",
 }
 
 REPLACEMENTS = {
-    "heading"        : "* #" ,
-    "italic"         : "/#/",
-    "inline_code"    : "~#~",
-    "basic_link"     : "[#]",
-    "file_link"      : "[#]",
-    "code_block"     : "begin_src\n#\nend_src",
+    "heading"              : "* #" ,
+    "italic"               : "/#/",
+    "inline_code"          : "~#~",
+    "basic_link"           : "[#]",
+    "link_with_description": "[#]", # TODO
+    "file_link"            : "[#]",
+    "code_block"           : "begin_src #\rend_src",
 }
 
 REPLACEMENT_PLACEHOLDER = "#"
 
-
 # generate headings based on level
 def generate_header(header_txt, level):
-    res = header_txt.replace("=",'')
-    final = REPLACEMENTS["heading"].replace(REPLACEMENT_PLACEHOLDER, res)
+    # If the level was > 1, theres a change there are still some vimwiki headers left
+    txt = header_txt.replace("=",'')
+    final = REPLACEMENTS["heading"].replace(REPLACEMENT_PLACEHOLDER, txt)
+
+    # If the level is one, no need for more * being added to the right.
+    # The line above is the result we want.
     if level == 1:
         return final
+
     # Because the replacement template has a * already in it, we have to remove 1 level
     adjusted_level = (level - 1)
     return final.rjust(len(final) + adjusted_level ,"*")
@@ -83,71 +61,18 @@ def applyReplacement(text, replacementType):
     if replacementType == "heading":
         return generate_header_replacement(text)
 
-    print(replacementType)
     return REPLACEMENTS[replacementType].replace(REPLACEMENT_PLACEHOLDER, text)
 
-def getInnerText(rgx, mk_type, text):
+def perform(rgx, mk_type, text):
     matches = re.finditer(rgx, text, re.MULTILINE)
     for matchNum, match in enumerate(matches, start=1):
         print ("Original => {match} ".format(match = match.group(0)))
         print(applyReplacement(match.group(1), mk_type))
-#        if mk_type == "heading":
-#            hding_level = find_header_level(match.group(0))
-#            print(generate_header(match.group(1), hding_level))
-#            return generate_header(match.group(1), hding_level)
-#            applyReplacement(match.group(0), mk_type)
-#        else:
-#            applyReplacement(match.group(1), mk_type)
-#        print(applyReplacement(match.group(1), mk_type))
-
-
-# Headings
-#==============================
-test_text = '=Chapter one='
-#print(getInnerText(heading, test_text))
-
-# Text Styles
-###############################
-# Italic
-test_text = "_and So it goes_"
-#print(getInnerText(italic, test_text))
-
-# Inline code
-test_text= "`echo 'Hello'`"
-#print(getInnerText(inline_code, test_text))
-
-# Links
-###############################
-#
-# Basic
-test_text = "[[www.link_to_somewhere.com]]"
-#print(getInnerText(basic_link, test_text))
-
-## with description
-test_text = "[[www.link_to_somewhere.com | a link to somewhere]]"
-#print(getInnerText(link_with_description, test_text))
-
-## file
-test_text = "{{file:./some-file.png}}"
-#print(getInnerText(file_link, test_text))
-
-## Code block
-test_text = """
-{{{python
-  print()
-  def funk():
-    doTheThing()
-}}}
-"""
-
-#print(getInnerText(code_block, test_text))
 
 ## Read a file, save it's contents to a string var
 file_text = ""
-with open("./TestHeaders.wiki", 'r') as f:
+with open("./BasicMarkup.wiki", 'r') as f:
     file_text = f.read()
 
-getInnerText(heading,"heading",file_text)
-
-#for markdown_type, regex in REGEX.items():
-#    getInnerText(regex, markdown_type, file_text)
+for markdown_type, regex in REGEX.items():
+    perform(regex, markdown_type, file_text)
