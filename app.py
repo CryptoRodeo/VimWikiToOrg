3653db8b17f81fc85cbe71243be27f68615a4e7f
Bryan Ramos <bryan.ramos@hey.com>
2022-12-02 20:00:55 -0500

    Mostly working substitutions now
    Note:
    - some markup might need work like links

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import re
 
 REGEX = {
-    "heading"              : "^=(.*)={1,}$",
+#    "heading"              : "^=(.*)={1,}$", # original
+    "heading"              : "^=*(.*[^=])(={1,6})$",
     "italic"               : "_(.*)_",
     "inline_code"          : "`(.*)`",
     "basic_link"           : "\[\[(.*)\]\]",
@@ -14,16 +15,18 @@ REPLACEMENTS = {
     "heading"              : "* #" ,
     "italic"               : "/#/",
     "inline_code"          : "~#~",
+    # TODO - links might need some work?
     "basic_link"           : "[#]",
-    "link_with_description": "[#]", # TODO
+    "link_with_description": "[#]",
     "file_link"            : "[#]",
+    # TODO - verify
     "code_block"           : "begin_src #\rend_src",
 }
 
 REPLACEMENT_PLACEHOLDER = "#"
 
 # generate headings based on level
-def generate_header(header_txt, level):
+def pad_header(header_txt, level):
     # If the level was > 1, theres a change there are still some vimwiki headers left
     txt = header_txt.replace("=",'')
     final = REPLACEMENTS["heading"].replace(REPLACEMENT_PLACEHOLDER, txt)
@@ -38,41 +41,62 @@ def generate_header(header_txt, level):
     return final.rjust(len(final) + adjusted_level ,"*")
 
 def find_header_level(txt):
+    # Worst case, return a top level heading.
     level = 1
-    for x in range(1,7):
-        # Adjust regex to check for different header levels
-        rgx = ("^=(.*)={%s}$" % x)
+    for x in range(1,8):
         level = x
-        # Keep going until we no longer have a match, return the last match.
-        if not re.match(rgx, txt):
+        rgx = ("^(={%s})$" % level)
+
+        # Keep going until we find a match
+        if re.match(rgx, txt, re.MULTILINE):
             return level
 
-    # Worst case, return a top level heading.
     return level
+
+def get_header_level_regex(level):
+    return ("^=*(.*[^=])(={%s})$" % level)
 
 def remove_whitespace(txt):
     return txt.replace(' ', '')
 
-def generate_header_replacement(text):
-    heading_level = find_header_level(text)
-    return generate_header(text, heading_level)
+def generate_header(inner_text, header_end):
+    heading_level = find_header_level(header_end)
+    rgx_for_level = get_header_level_regex(heading_level)
+    return pad_header(inner_text, heading_level)
 
-def applyReplacement(text, replacementType):
-    if replacementType == "heading":
-        return generate_header_replacement(text)
+def generate_replacement(text, replacement_type):
+    return REPLACEMENTS[replacement_type].replace(REPLACEMENT_PLACEHOLDER, text)
 
-    return REPLACEMENTS[replacementType].replace(REPLACEMENT_PLACEHOLDER, text)
+def apply_replacement(full_text, original_markup, replacement_markup):
+    return full_text.replace(original_markup, replacement_markup)
 
-def perform(rgx, mk_type, text):
-    matches = re.finditer(rgx, text, re.MULTILINE)
-    for matchNum, match in enumerate(matches, start=1):
-        print ("Original => {match} ".format(match = match.group(0)))
-        print(applyReplacement(match.group(1), mk_type))
+def apply_substitution(text, regex, match, replacement_type):
+    original_text = match.group(0)
+    inner_text = match.group(1)
+    if replacement_type == "heading":
+        heading_end = match.group(2)
+        replacement = generate_header(inner_text, heading_end)
+        return apply_replacement(text, original_text, replacement)
+
+    replacement = generate_replacement(inner_text, replacement_type)
+    return apply_replacement(text, original_text, replacement)
+
+def perform(text):
+    final_text = text
+    for markdown_type, regex in REGEX.items():
+        matches = re.finditer(regex, text, re.MULTILINE)
+        for matchNum, match in enumerate(matches, start=1):
+            final_text = apply_substitution(final_text, regex, match, markdown_type)
+    return final_text
 
 ## Read a file, save it's contents to a string var
 file_text = ""
 with open("./BasicMarkup.wiki", 'r') as f:
     file_text = f.read()
 
-for markdown_type, regex in REGEX.items():
-    perform(regex, markdown_type, file_text)
+final_text = file_text
+
+print(perform(file_text))
+
+# NOTE:
+# - I think italic conversion is broken.
