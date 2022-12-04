a701d21c8c8b097628dfee87acb5654e052e04cb
Bryan Ramos <bryan.ramos@hey.com>
2022-12-04 18:30:59 -0500

    Move files out to another folder, update references

---
+++ vimwiki_to_org/src/converters/helpers/org_markdown.py
@@ -0,0 +1,11 @@
+PLACEHOLDER = "<^>"
+DESCRIPTION_PLACEHOLDER = "<->"
+ORG_MARKDOWN = {
+    "heading"              : f"{PLACEHOLDER}" ,
+    "italic"               : f"/{PLACEHOLDER}/",
+    "inline_code"          : f"~{PLACEHOLDER}~",
+    "wiki_link"            : f"[[file:{PLACEHOLDER}]]",
+    "file_link"            : f"[[{PLACEHOLDER}]]",
+    "link_with_description": f"[[{PLACEHOLDER}][{DESCRIPTION_PLACEHOLDER}]]",
+    "code_block"           : f"#+begin_src {PLACEHOLDER}\n#+end_src",
+}
--- vimwiki_to_org/src/transformers/helpers/wiki_regex.py
+++
@@ -1,9 +0,0 @@
-REGEX = {
-    "heading"    : "^=*(.*[^=])(={1,6})$",
-    "italic"     : "_(.*)_",
-    "inline_code": "`(.*)`",
-    "wiki_link"       : "\[\[(.*)\]\]",
-    "link_with_description": "\[\[(.*)\|(.*)\]\]",
-    "file_link"  : "\{{2}(file:.*)\}{2}",
-    "code_block" : "\{{3}([^}]*)\}{3}",
-}
