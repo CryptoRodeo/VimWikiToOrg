20db6674b2814fc938e5cd90fb77a06e23ebcf6f
Bryan Ramos <bryan.ramos@hey.com>
2022-12-05 18:07:04 -0500

    WIP update regex
    - add regex for bold text
    - update asterisk list item regex

--- vimwiki_to_org/src/converters/helpers/wiki_regex.py
+++ vimwiki_to_org/src/converters/helpers/wiki_regex.py
@@ -1,9 +1,11 @@
 REGEX = {
     "heading"              : "^=*(.*[^=])(={1,6})$",
+    "bold_text"            : "\*(.*)\*",
     "inline_code"          : "\`(.*)\`",
     "wiki_link"            : "\[\[(.*)\]\]",
     "link_with_description": "\[\[(.*)\|(.*)\]\]",
     "file_link"            : "\{{2}(file:.*)\}{2}",
     "code_block"           : "\{{3}((\n|.)*?)\}{3}",
-    "asterisk_list_item"   : "^\*([^\*])+$",
+#    "asterisk_list_item"   : "^\*([^\*])+$",
+    "asterisk_list_item"   : "^\*(.+([^\*])$)",
 }
