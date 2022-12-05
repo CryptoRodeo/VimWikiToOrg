42477c3c2fde0dd6237af0175ad601a47357c5b3
Bryan Ramos <bryan.ramos@hey.com>
2022-12-05 07:06:16 -0500

    Use f string

--- vimwiki_to_org/__main__.py
+++ vimwiki_to_org/__main__.py
@@ -2,5 +2,5 @@ from src.app import App
 import os
 
 if __name__ == "__main__":
-    path = "/home/{user}/vimwiki/".format(user=os.getlogin())
+    path = f"/home/{os.getlogin()}/vimwiki/"
     App(path).run()
