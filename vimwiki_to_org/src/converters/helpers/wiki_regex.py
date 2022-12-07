d1ed2d7a282aa80179a9436651532963620735c8
Bryan Ramos <bryan.ramos@hey.com>
2022-12-06 20:58:17 -0500

    Single out regex so we can use them by priority

--- vimwiki_to_org/src/converters/helpers/wiki_regex.py
+++ vimwiki_to_org/src/converters/helpers/wiki_regex.py
@@ -1,3 +1,26 @@
+HEADING_REGEX = {
+     "heading"              : "^=*(.*[^=])(={1,6})$",
+}
+
+MULTILINE_REGEX = {
+    "code_block"           : "\{{3}((\n|.)*?)\}{3}",
+}
+
+LIST_REGEX = {
+     "asterisk_list_item"   : "^\*(.+([^\*])$)",
+}
+
+LINK_REGEX = {
+    "wiki_link"            : "\[\[(.*)\]\]",
+    "link_with_description": "\[\[(.*)\|(.*)\]\]",
+     "file_link"            : "\{{2}(file:.*)\}{2}",
+}
+
+TEXT_FORMATTING_REGEX = {
+    "bold_text"            : "(\*(.*)\*$)",
+    "inline_code"          : "\`(.*)\`",
+}
+
 REGEX = {
     "heading"              : "^=*(.*[^=])(={1,6})$",
     "bold_text"            : "\*(.*)\*",
