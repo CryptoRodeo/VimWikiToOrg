cea877505b70989b7adcd47dd620af2086502292
Bryan Ramos <bryan.ramos@hey.com>
2022-12-07 07:27:23 -0500

    Remove old regex

--- vimwiki_to_org/src/converters/helpers/wiki_regex.py
+++ vimwiki_to_org/src/converters/helpers/wiki_regex.py
@@ -29,14 +29,3 @@ REGEX_BY_PRIORITY = [
     LIST_REGEX,
     TEXT_FORMATTING_REGEX,
 ]
-
-REGEX = {
-    "heading"              : "^=*(.*[^=])(={1,6})$",
-    "bold_text"            : "\*(.*)\*",
-    "inline_code"          : "\`(.*)\`",
-    "wiki_link"            : "\[\[(.*)\]\]",
-    "link_with_description": "\[\[(.*)\|(.*)\]\]",
-    "file_link"            : "\{{2}(file:.*)\}{2}",
-    "code_block"           : "\{{3}((\n|.)*?)\}{3}",
-    "asterisk_list_item"   : "^\*(.+([^\*])$)",
-}
