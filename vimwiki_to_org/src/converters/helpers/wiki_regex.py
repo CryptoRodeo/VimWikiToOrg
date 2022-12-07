c0dcad22987b803deadcd08454f5e64907a0523f
Bryan Ramos <bryan.ramos@hey.com>
2022-12-06 21:11:23 -0500

    Flake8 changes

--- vimwiki_to_org/src/converters/helpers/wiki_regex.py
+++ vimwiki_to_org/src/converters/helpers/wiki_regex.py
@@ -1,24 +1,24 @@
 HEADING_REGEX = {
-     "heading"              : "^=*(.*[^=])(={1,6})$",
+    "heading" : "^=*(.*[^=])(={1,6})$",
 }
 
 MULTILINE_REGEX = {
-    "code_block"           : "\{{3}((\n|.)*?)\}{3}",
+    "code_block" : "\{{3}((\n|.)*?)\}{3}",
 }
 
 LIST_REGEX = {
-     "asterisk_list_item"   : "^\*(.+([^\*])$)",
+    "asterisk_list_item" : "^\*(.+([^\*])$)",
 }
 
 LINK_REGEX = {
-    "wiki_link"            : "\[\[(.*)\]\]",
+    "wiki_link" : "\[\[(.*)\]\]",
     "link_with_description": "\[\[(.*)\|(.*)\]\]",
-     "file_link"            : "\{{2}(file:.*)\}{2}",
+    "file_link" : "\{{2}(file:.*)\}{2}",
 }
 
 TEXT_FORMATTING_REGEX = {
-    "bold_text"            : "(\*(.*)\*$)",
-    "inline_code"          : "\`(.*)\`",
+    "bold_text" : "(\*(.*)\*$)",
+    "inline_code" : "\`(.*)\`",
 }
 
 REGEX = {
