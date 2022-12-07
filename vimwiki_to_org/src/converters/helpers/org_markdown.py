88eee4e0ea7f32e5a786db2eb94fbd611599ca4e
Bryan Ramos <bryan.ramos@hey.com>
2022-12-06 21:28:31 -0500

    Bring Back Italics

--- vimwiki_to_org/src/converters/helpers/org_markdown.py
+++ vimwiki_to_org/src/converters/helpers/org_markdown.py
@@ -4,6 +4,7 @@ ORG_MARKDOWN = {
     "heading"              : f"{PLACEHOLDER}",
     "bold_text"            : f"{PLACEHOLDER}",
     "inline_code"          : f"{PLACEHOLDER}",
+    "italic_text"          : f"{PLACEHOLDER}",
     "wiki_link"            : f"[[file:{PLACEHOLDER}]]",
     "file_link"            : f"[[{PLACEHOLDER}]]",
     "link_with_description": f"[[{PLACEHOLDER}][{DESCRIPTION_PLACEHOLDER}]]",
