4b6f3fa0759e92ae9b44910d41b237b6816b99a7
Bryan Ramos <30709212+CryptoRodeo@users.noreply.github.com>
2022-12-08 08:44:13 -0500

    Package app (#3)
    * Update gitignore
    
    * Add packaging files
    
    * Move test files into vimwiki_to_org dir
    
    * Remove old test files
    
    * Update __main__.py, use strict imports
    
    * update README.md
    
    * Update tox.ini files to point to new dirs
    
    * Flake8 change
    
    Co-authored-by: Bryan Ramos <bryan.ramos@hey.com>

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from os import walk
+from os.path import join, relpath
+
+from setuptools import find_packages, setup
+
+
+version = "1.0.5"
+
+entry_points = {
+    'console_scripts': [
+        'vimwki_to_org = vimwiki_to_org.__main__:main'
+    ]
+}
+
+README = open('README.md', encoding='utf-8').read()
+
+description = README
+
+setup(
+    name='vimwiki_to_org',
+    version=version,
+    url='https://github.com/CryptoRodeo/VimWikiToOrg',
+    author='Bryan Ramos',
+    author_email='bryan.ramos@hey.com',
+    description="Converts vimwiki files to org mode",
+    project_urls={
+        'Source': 'https://github.com/CryptoRodeo/VimWikiToOrg',
+        'Tracker': 'https://github.com/CryptoRodeo/VimWikiToOrg/issues',
+    },
+    keywords='vimwiki orgmode convert',
+    license='GPLv3',
+    long_description=description,
+    packages=find_packages(),
+    entry_points=entry_points,
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    ],
+    test_suite='vimwiki_to_org.tests',
+)
