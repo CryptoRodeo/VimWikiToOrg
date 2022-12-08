066274e51823bc7aa2676793eecac7d322e82dcd
Bryan Ramos <bryan.ramos@hey.com>
2022-12-08 08:35:33 -0500

    Remove old test files

--- tests/helpers/prevention_tag.py
+++
@@ -1,4 +0,0 @@
-from vimwiki_to_org.src.converters.helpers.prevention_tag import PREVENTION_TAG
-
-def prevention_tag():
-    return PREVENTION_TAG
--- tests/helpers/text_conversion.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-from vimwiki_to_org.src.converters.vimwiki_to_org import convert
-
-def apply_conversion(txt):
-    return convert(txt)
